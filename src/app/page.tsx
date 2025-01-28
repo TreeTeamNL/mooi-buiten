af4f0a55c3fcc069a570610d9b82783128f09c7b
Alina <alina100chuk@gmail.com>
2025-01-28 13:12:49 +0100

    style: fix position banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <>
       <div className={styles.wrapper}>
-        <div className={styles.box}>
+        <div className={styles.bannerImage1}>
           <Image
             src="/ice.jpg"
             alt="Picture of the author"
@@ -13,8 +13,7 @@ export default function Home() {
             height={550}
           />
         </div>
-        <div className={styles.box}>
-          {" "}
+        <div className={styles.bannerImage2}>
           <Image
             src="/meer.jpg"
             alt="Picture of the author"
@@ -22,28 +21,23 @@ export default function Home() {
             height={550}
           />
         </div>
-        <div className={styles.box}>
+        <div className={styles.bannerImage3}>
           <Image
             src="/boom.jpg"
             alt="Picture of the author"
-            width={457}
+            width={459}
             height={550}
           />
         </div>
         <div className={styles.box}>d</div>
-        <div className={styles.banner}>
-          <q className={styles.bannerTypography}>
+        <div className={styles.sectionQuote}>
+          <q className={styles.quoteTypography}>
             Nature is not a place to visit. It is home.
           </q>
           <p style={{ fontSize: 20 }}>– Gary Snyder</p>
         </div>
         <div className={styles.box}>f</div>
         <div className={styles.box}>g</div>
-        <div className={styles.box}>h</div>
-        <div className={styles.box}>i</div>
-        <div className={styles.box}>k</div>
-        <div className={styles.box}>l</div>
-        <div className={styles.box}>l</div>
       </div>
     </>
   );
