248031d532c0d5103ce5d56e30721b07bc3bfca9
Alina <alina100chuk@gmail.com>
2025-02-26 14:36:42 +0100

    feat: validation messages

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,60 +1,83 @@
 "use client";
 import Form from "next/form";
 import styles from "./Contact.module.scss";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type Inputs = {
+  fname: string;
+  lname: string;
+  subject: string;
+  message: string;
+};
 
 export default function Contact() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  // console.log(errors);
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+
   return (
     <>
       <div className={styles.contactPage}>
         <div className={styles.contactForm}>
-          <Form action="/search">
+          <Form action="/search" onSubmit={handleSubmit(onSubmit)}>
             <h2>Contactformulier</h2>
             <div className={styles.controlLabelstar}>
               <strong>*</strong>
               Verplicht veld
             </div>
 
-            <div className={styles.formRow}>
+            <div className={styles.inputGroup}>
               <label htmlFor="fname" className={styles.required}>
                 Naam
               </label>
               <input
                 type="text"
                 id="fname"
-                name="fname"
+                {...register("fname", { required: "Dit veld is verplicht" })}
                 placeholder="jouw naam"
               ></input>
+              {errors.fname && <span>{errors.fname.message}</span>}
             </div>
-            <div className={styles.formRow}>
+            <div className={styles.inputGroup}>
               <label htmlFor="lname" className={styles.required}>
                 E-mailadres
               </label>
               <input
                 type="text"
                 id="lname"
-                name="lname"
+                {...register("lname", { required: "Dit veld is verplicht" })}
                 placeholder="jouw e-mailadres"
               ></input>
+              {errors.lname && <span>{errors.lname.message}</span>}
             </div>
-            <div className={styles.formRow}>
+            <div className={styles.inputGroup}>
               <label htmlFor="subject" className={styles.required}>
                 Onderwerp
               </label>
               <input
                 type="text"
                 id="subject"
-                name="subject"
+                {...register("subject", {
+                  required: "Dit veld is verplicht",
+                  minLength: { value: 5, message: "Vul minstens 5 letters in" },
+                })}
                 placeholder="Onderwerp"
               ></input>
+              {errors.subject && <span>{errors.subject.message}</span>}
             </div>
-            <div>
+            <div className={styles.inputGroup}>
               <label htmlFor="message" className={styles.required}>
                 Bericht
               </label>
               <textarea
                 id="message"
-                name="message"
                 placeholder="Schrijf hier je bericht.."
+                {...register("message")}
               ></textarea>
             </div>
             <button type="submit">Verstuur e-mail</button>
