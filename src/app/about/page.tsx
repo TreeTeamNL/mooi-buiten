001cf862fbc215dcd8d64b5729bdedb17b4067f3
Alina <alina100chuk@gmail.com>
2025-02-26 15:53:41 +0100

    feat: add contant about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,39 @@
 "use client";
+import styles from "./about.module.scss";
+import Image from "next/image";
 
 export default function About() {
-  return <p>About test</p>;
+  return (
+    <>
+      <div className={styles.about}>
+        <div className={styles.contentWrapper}>
+          <div className={styles.text}>
+            <h1>Over mij</h1>
+            <p>
+              Lorem ipsum odor amet, consectetuer adipiscing elit. Risus sit
+              mauris non class tempus lobortis etiam sem himenaeos. Sociosqu
+              euismod penatibus mattis inceptos tincidunt facilisi fusce erat.
+            </p>
+            <p>
+              Vehicula feugiat ultricies dui ad rutrum venenatis. Class mauris
+              dictum magna rhoncus habitant potenti posuere nostra. Blandit
+              litora metus nisl tempus eros ridiculus finibus volutpat. Lacinia
+              ante semper habitasse malesuada risus. Litora penatibus erat augue
+              mus velit congue. Nec lacinia vel porttitor, fermentum id
+              facilisis ipsum sagittis.
+            </p>
+          </div>
+
+          <div className={styles.image}>
+            <Image
+              src="/muscari-vierkant.jpg"
+              alt="Picture of the author"
+              width={400}
+              height={400}
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
