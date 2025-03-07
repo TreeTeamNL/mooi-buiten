67338f078b99acd9d105428a13edad67b77becf4
Alina <alina100chuk@gmail.com>
2025-03-07 16:46:55 +0100

    WIP: layout home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,27 +13,22 @@ export default function Home() {
   return (
     <>
       <MainRow>
-        <div className={styles.bgGrey}>
+        <div>
           <MainInner>
-            <h1>Over mij</h1>
-            <p>
-              Lorem ipsum odor amet, consectetuer adipiscing elit. Risus sit
-              mauris non class tempus lobortis etiam sem himenaeos. Sociosqu
-              euismod penatibus mattis inceptos tincidunt facilisi fusce erat.
-            </p>
-            <p>
-              Vehicula feugiat ultricies dui ad rutrum venenatis. Class mauris
-              dictum magna rhoncus habitant potenti posuere nostra. Blandit
-              litora metus nisl tempus eros ridiculus finibus volutpat. Lacinia
-              ante semper habitasse malesuada risus. Litora penatibus erat augue
-              mus velit congue. Nec lacinia vel porttitor, fermentum id
-              facilisis ipsum sagittis.
-            </p>
+            <div className={styles.containerFlex}>
+              <div className={styles.contantCard}>
+                <p>1</p>
+              </div>
+              <div className={styles.contantCard}>
+                <p>2</p>
+              </div>
+              <div className={styles.contantCard}>3</div>
+            </div>
           </MainInner>
         </div>
       </MainRow>
       <MainRow>
-        <div>
+        <div className={styles.bgGrey}>
           <MainInner>
             <h1>Over mij</h1>
             <p>
