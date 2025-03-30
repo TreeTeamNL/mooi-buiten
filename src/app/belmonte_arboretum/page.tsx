6c81fa4cae9e1895b15daa04e8a02e78b9963989
Alina <alina100chuk@gmail.com>
2025-03-30 18:41:53 +0200

    responsive: belmonte arboretum page

--- src/app/belmonte_arboretum/page.tsx
+++ src/app/belmonte_arboretum/page.tsx
@@ -10,11 +10,10 @@ export default function pagebelmontearboretum() {
         <div className={styles.wrapper}>
           <div className={styles.contentTitle}>
             <h3>Belmonte Arboretum in februari</h3>
-            <p>...</p>
           </div>
-
           <div className={styles.img1}>
             <Image
+              className={styles.img}
               src="/202502_BelmonteArboretum/202502_belmonteArboretum_1.jpg"
               alt="Picture of the author"
               width={1009}
@@ -24,6 +23,7 @@ export default function pagebelmontearboretum() {
 
           <div className={styles.img2}>
             <Image
+              className={styles.img}
               src="/202502_BelmonteArboretum/202502_belmonteArboretum_2.jpg"
               alt="Picture of the author"
               width={500}
@@ -32,6 +32,7 @@ export default function pagebelmontearboretum() {
           </div>
           <div className={styles.img3}>
             <Image
+              className={styles.img}
               src="/202502_BelmonteArboretum/202502_belmonteArboretum_3.jpg"
               alt="Picture of the author"
               width={500}
@@ -40,6 +41,7 @@ export default function pagebelmontearboretum() {
           </div>
           <div className={styles.img4}>
             <Image
+              className={styles.img}
               src="/202502_BelmonteArboretum/202502_belmonteArboretum_4.jpg"
               alt="Picture of the author"
               width={500}
@@ -48,6 +50,7 @@ export default function pagebelmontearboretum() {
           </div>
           <div className={styles.img5}>
             <Image
+              className={styles.img}
               src="/202502_BelmonteArboretum/202502_belmonteArboretum_5.jpg"
               alt="Picture of the author"
               width={500}
@@ -56,6 +59,7 @@ export default function pagebelmontearboretum() {
           </div>
           <div className={styles.img6}>
             <Image
+              className={styles.img}
               src="/202502_BelmonteArboretum/202502_belmonteArboretum_6.jpg"
               alt="Picture of the author"
               width={500}
@@ -64,6 +68,7 @@ export default function pagebelmontearboretum() {
           </div>
           <div className={styles.img7}>
             <Image
+              className={styles.img}
               src="/202502_BelmonteArboretum/202502_belmonteArboretum_7.jpg"
               alt="Picture of the author"
               width={500}
@@ -72,6 +77,7 @@ export default function pagebelmontearboretum() {
           </div>
           <div className={styles.img8}>
             <Image
+              className={styles.img}
               src="/202502_BelmonteArboretum/202502_belmonteArboretum_8.jpg"
               alt="Picture of the author"
               width={1009}
